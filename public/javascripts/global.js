324d098d0054c0c908e0025a3d527fecd22e1263
Ryan Holmes <rholmes@Dylan-Harnesss-MacBook-Pro.local>
2015-06-09 11:56:50 -0400

    introducing... EXTJS

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,5 +1,21 @@
+$(document).ready(function(){
+
+});
+
+function test(){
+	var p = Ext.create('Ext.panel.Panel', {
+	    title: 'Hello',
+	    width: 200,
+	    html: '<p>World!</p>',
+	    renderTo: Ext.getBody()
+	});
+}
 
 //called when about butto is clicked
 function aboutButton(){
 	console.log("About");
 }
+
+$('#button1').on('click', function () {
+	console.log("hey");
+});
